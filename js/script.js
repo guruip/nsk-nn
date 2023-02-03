69ae295cdcfc3b6703d17fe74977710cb7ff30e4
guruip <82904567+guruip@users.noreply.github.com>
2023-02-03 15:28:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+if (window.innerWidth <= 620) {
+	document.getElementById("loggo").src = "./img/main-phone.png";
+	
+  }else {
+		document.getElementById("loggo").src = "./img/main.png";
+}
+
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows:false,
