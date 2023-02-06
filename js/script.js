8dbe9fa73a882d288107952d21cf64435ca458ad
guruip <82904567+guruip@users.noreply.github.com>
2023-02-06 21:54:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+/*ниже код для выбора главного фото*/
 if (window.innerWidth <= 620) {
 	document.getElementById("loggo").src = "./img/main-phone.png";
 	
   }else {
 		document.getElementById("loggo").src = "./img/main.png";
 }
-
+/*ниже код для слайдера*/
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows:false,
@@ -22,4 +23,22 @@ $(document).ready(function(){
 			},
 		]
 	});
+});
+/*ниже код для первой кнопки чтобы скролила к форме*/
+const $button = document.getElementById('formscroll');
+const $form = document.getElementById('wrapeers');
+$button.addEventListener('click', e => {
+  $form.scrollIntoView({ 
+    block: 'nearest',
+    behavior: 'smooth',
+  });
+});
+/*ниже код для кнопки "подробнее о нас" чтобы скролила к Портфолио*/
+const $butt = document.getElementById('aboutus');
+const $for = document.getElementById('wrapscroll');
+$butt.addEventListener('click', e => {
+  $for.scrollIntoView({ 
+    block: 'nearest',
+    behavior: 'smooth',
+  });
 });
